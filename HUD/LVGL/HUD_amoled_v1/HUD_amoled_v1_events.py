15a6401dc820ad73f838f29f5a4e090ae40bfd31
yuhkm <yuhkm121@gmail.com>
2025-08-08 18:14:07 +0900

    Fix: re-add HUD folder as regular directory

---
+++ HUD/LVGL/HUD_amoled_v1/HUD_amoled_v1_events.py
@@ -0,0 +1,20 @@
+
+def ui_btnSetting_event_handler(event_struct):
+    return
+
+
+def ui_contSetting_event_handler(event_struct):
+    return
+
+
+def ui_btnSetL_event_handler(event_struct):
+    return
+
+
+def ui_btnSetR_event_handler(event_struct):
+    return
+
+
+def ui_btnSave_event_handler(event_struct):
+    return
+
